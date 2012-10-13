1c9cffeb1a8b75abf1810e2bef3949aba3e86a2b
Ben Casey <bcasey@mines.edu>
2012-10-13 15:12:47 -0600

    Intital add

---
+++ gitMergePart2/src/HotTub.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class HotTub {
+	private int temp;
+	private int numJets;
+	private int size;
+	
+	public HotTub() {
+		super();
+		// TODO Auto-generated constructor stub
+		numJets = 5;
+		size = 50;
+		temp =100;
+	}
+	
+	public HotTub(int temp, int numJets, int size) {
+		super();
+		this.temp = temp;
+		this.numJets = numJets;
+		this.size = size;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("what's the temp of spa?");
+		String temp = scan.next();
+		System.out.println("what's the size of spa?");
+		String tSize = scan.next();
+		System.out.println("what's the number of Jets of spa?");
+		String numJ = scan.next();
+		HotTub ht = new HotTub(Integer.parseInt(temp), Integer.parseInt(tSize), Integer.parseInt(numJ));
+	}
+
+}
