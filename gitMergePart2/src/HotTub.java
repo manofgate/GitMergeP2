a0dc41a1bc57cd34ebac44a65d01a17a0a55007f
Ben Casey <bcasey@mines.edu>
2012-10-13 23:39:02 -0600

    print statment

--- gitMergePart2/src/HotTub.java
+++ gitMergePart2/src/HotTub.java
@@ -12,21 +12,25 @@ public class HotTub {
 		this.numJets = numJets;
 		this.size = size;
 	}
-
+	public void print(){
+		System.out.println("temp:" + temp );
+		System.out.println("numJets: " + numJets);
+		System.out.println("Size: " + size);
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		System.out.println("what's the temp of spa?");
+		System.out.println("what's the temp of pool?");
 		String temp = scan.next();
-		System.out.println("what's the size of spa?");
+		System.out.println("what's the size of pool?");
 		String tSize = scan.next();
 		System.out.println("what's the number of Jets of spa?");
 		String numJ = scan.next();
 		HotTub ht = new HotTub(Integer.parseInt(temp), Integer.parseInt(tSize), Integer.parseInt(numJ));
-		
+		ht.print();
 	}
 
 }
