1a50a145823b61c12e68c38396dca8363279dc20
Ben Casey <bcasey@mines.edu>
2012-10-13 23:33:42 -0600

    abstractedMain

--- gitMergePart2/src/HotTub.java
+++ gitMergePart2/src/HotTub.java
@@ -16,17 +16,6 @@ public class HotTub {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		System.out.println("what's the temp of spa?");
-		String temp = scan.next();
-		System.out.println("what's the size of spa?");
-		String tSize = scan.next();
-		System.out.println("what's the number of Jets of spa?");
-		String numJ = scan.next();
-		HotTub ht = new HotTub(Integer.parseInt(temp), Integer.parseInt(tSize), Integer.parseInt(numJ));
-		
-	}
+	
 
 }
